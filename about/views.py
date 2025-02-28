0a138f1cac9b145ccd48f61f3353d4ffa1f3dc25
shantaisat <shanta.mahoneyis@gmail.com>
2025-02-28 15:53:32 +0100

    added hello world and about

---
+++ about/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def about_view(request):
+	return render(request, 'about/about.html')
---
+++ hello_world/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+
+
+def index(request):
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
